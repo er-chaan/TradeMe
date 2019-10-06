4edd94e5c31eb5aa8ab904fdf5ad2b760ffdef66
er-chaan <er.chandreshbhai@gmail.com>
2019-10-06 14:41:14 +0530

    inplay select2

--- app/src/app/user/inplay/inplay.component.ts
+++ app/src/app/user/inplay/inplay.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+declare var jquery:any;
+declare var $ :any;
 
 @Component({
   selector: 'app-inplay',
@@ -10,6 +12,7 @@ export class InplayComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $('.select2').select2();
   }
 
 }
