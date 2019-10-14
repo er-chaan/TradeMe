df619846bccfb86ec66b0e37f821b5cc0b106dcd
er-chaan <er.chandreshbhai@gmail.com>
2019-10-15 00:21:17 +0530

    db update

--- api/TradeMe.sql
+++ api/TradeMe.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Oct 12, 2019 at 10:24 PM
+-- Generation Time: Oct 15, 2019 at 12:20 AM
 -- Server version: 10.1.37-MariaDB-3
 -- PHP Version: 7.2.4-1+b2
 
@@ -23,6 +23,35 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `accounts`
+--
+
+CREATE TABLE `accounts` (
+  `id` int(11) NOT NULL,
+  `mobile` varchar(10) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `description` text NOT NULL,
+  `debit` float NOT NULL DEFAULT '0',
+  `credit` float NOT NULL DEFAULT '0',
+  `balance` float NOT NULL,
+  `status` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `accounts`
+--
+
+INSERT INTO `accounts` (`id`, `mobile`, `date`, `description`, `debit`, `credit`, `balance`, `status`) VALUES
+(5, '9004313006', '2019-10-14 18:20:45', 'payin 10 processed ', 0, 10, 10, 'payin'),
+(6, '9004313006', '2019-10-14 18:29:18', 'payin 15 processed ', 0, 15, 25, 'payin'),
+(7, '9004313006', '2019-10-14 18:34:09', 'payout 5 processed ', 5, 0, 20, 'payout'),
+(8, '9004313006', '2019-10-14 18:34:45', 'payout 20 processed ', 20, 0, 0, 'payout'),
+(9, '9004313006', '2019-10-14 18:45:57', 'payin 100 processed ', 0, 100, 100, 'payin'),
+(10, '9004313006', '2019-10-14 18:48:41', 'payin 990 processed ', 0, 990, 1090, 'payin');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -32,6 +61,7 @@ CREATE TABLE `users` (
   `email` varchar(100) DEFAULT NULL,
   `password` text,
   `token` text,
+  `balance` float DEFAULT '0',
   `lastUpdated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `status` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -40,12 +70,18 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `mobile`, `email`, `password`, `token`, `lastUpdated`, `status`) VALUES
-(61, '9004313006', 'er.chandreshbhai@gmail.com', 'bac59658e712105440b9a0853770eca3', '6542b0159b9deb5edaa5cf0ad5b8b95c05029f83', '2019-10-12 16:52:35', 'active');
+INSERT INTO `users` (`id`, `mobile`, `email`, `password`, `token`, `balance`, `lastUpdated`, `status`) VALUES
+(67, '9004313006', 'er.chandreshbhai@gmail.com', '16f6ee333087cc134afc201236048cd5', '168d65b478f8d6e2dc61e57134394ca7ab967484', 1090, '2019-10-14 18:48:41', 'active');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `accounts`
+--
+ALTER TABLE `accounts`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -60,10 +96,15 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `accounts`
+--
+ALTER TABLE `accounts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
